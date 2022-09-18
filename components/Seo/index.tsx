289ba1efa0d503e77d429791a3a8f38c43160d82
Nick Lima <nick.lima.wp@gmail.com>
2022-09-18 00:25:29 -0300

    fix seo title

--- components/Seo/index.tsx
+++ components/Seo/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head"
 
 const Seo = ({ pageTitle }: { pageTitle: string }) => (
   <Head>
-    <meta name="title" content={`${pageTitle} - NextJS Movie DB API`} />
+    <title>{`${pageTitle} - NextJS Movie DB API`}</title>
     <meta name="description" content="Movie DB API - Sample" />
   </Head>
 )
